28a01a09fad82fb6ea32c33e904ef2677569a336
AMAN <antilaman3113@gmail.com>
2023-08-09 18:41:17 +0530

    fix: error handling for unsplash api

--- pawpalace/src/Pages/ImageGallery.js
+++ pawpalace/src/Pages/ImageGallery.js
@@ -2,6 +2,11 @@ import React, { useEffect, useRef, useState } from "react";
 
 const ImageGallery = () => {
 	const [imageArr, setImageArr] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+	const [error, setError] = useState({
+		status: false,
+		message: "",
+	});
 	const scrollableRef = useRef(null);
 
 	const collections = 1270951;
@@ -9,9 +14,22 @@ const ImageGallery = () => {
 	const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=30&collections=${collections}`;
 
 	async function getDogImages() {
-		const resp = await fetch(apiUrl);
-		const data = await resp.json();
-		setImageArr((prev) => [...prev, ...data]);
+		try {
+			const resp = await fetch(apiUrl);
+			const data = await resp.json();
+
+			if (resp.ok) {
+				setImageArr((prev) => [...prev, ...data]);
+				setIsLoading(false);
+			}
+		} catch (err) {
+			setError({
+				status: true,
+				message:
+					"API Limit Reached. No more images could be shown. Checkout after an hr.",
+			});
+			setIsLoading(false);
+		}
 	}
 
 	if (imageArr.length > 200) {
@@ -21,10 +39,12 @@ const ImageGallery = () => {
 	function removeDogImages() {
 		const element = scrollableRef.current;
 
+		if (error.status) return;
+
 		if (element) {
 			const { scrollTop, scrollHeight, clientHeight } = element;
 
-			if (scrollTop + clientHeight + 2 >= scrollHeight) {
+			if (scrollTop + clientHeight + 3 >= scrollHeight) {
 				getDogImages();
 			}
 		}
@@ -51,6 +71,12 @@ const ImageGallery = () => {
 		<div className="h-full px-2">
 			<p className="h-[10%] text-center text-2xl font-bold">Image Gallery</p>
 
+			{isLoading && (
+				<p className="text-center font-bold text-3xl text-red-600">
+					Loading....
+				</p>
+			)}
+
 			<div ref={scrollableRef} className=" overflow-y-auto h-[90%] ">
 				<div className="columns-3 gap-2">
 					{imageArr?.map((image, idx) => (
@@ -62,6 +88,11 @@ const ImageGallery = () => {
 						/>
 					))}
 				</div>
+				{error.status && (
+					<p className="text-2xl font-bold text-gray-700 text-center p-2">
+						{error.message}
+					</p>
+				)}
 			</div>
 		</div>
 	);
